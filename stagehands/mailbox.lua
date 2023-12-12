f80d9ba88a6a8dedc3efb57663ad7085335c99df
Degranon <boba09@list.ru>
2023-12-12 18:04:08 +0100

    Replace with a stagehand that autocloses

---
+++ stagehands/mailbox.lua
@@ -0,0 +1,36 @@
+require "/stagehands/irden/irdencustomchathandler.lua"
+
+function init()
+  if Outbox then
+    self.outbox = Outbox.new("outbox", ContactList.new("contacts"))
+  end
+
+  -- That's new
+  iccstagehand_init()
+end
+
+function uninit()
+  self.outbox:uninit()
+end
+
+function update(dt)
+  if self.outbox then
+    self.outbox:update()
+
+    if self.outbox:empty() or not(iccstagehand_update) then
+      stagehand.die()
+    end
+  end
+  
+  iccstagehand_update(dt)
+end
+
+function post(contacts, messages)
+  if self.outbox then
+    self.outbox.contactList:registerContacts(contacts)
+    for _,messageData in ipairs(messages) do
+      self.outbox:logMessage(messageData, "mailbox received")
+      self.outbox:postpone(messageData)
+    end
+  end
+end
