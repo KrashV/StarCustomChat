3f4bb4edb70606eb39261b7ab87f2cc52139c2d0
Degranon <boba09@list.ru>
2024-03-17 15:34:21 +0100

    Fixed voice chat reenabling on changing the chat box size

--- interface/scripted/starcustomchat/plugins/voicechat/voicechat.lua
+++ interface/scripted/starcustomchat/plugins/voicechat/voicechat.lua
@@ -8,7 +8,15 @@ function voicechat:init()
   self:_loadConfig()
   local isEnabled = root.getConfiguration("scc_voice_enabled") or false
   widget.setChecked("btnCkVoice", isEnabled)
-  voice.setEnabled(isEnabled)
+
+  -- This code is a little hideous because voice.setEnabled(true) closes the audio device and opens it anew. We don't want stutters in our audio
+  if isEnabled then 
+    if not voice.enabled() then
+      voice.setEnabled(true)
+    end
+  else
+    voice.setEnabled(false)
+  end
 end
 
 function voicechat:onModeToggle(button, isChecked)
