bd0dd4e71b313a63eb74a51ea5f93aabbad0b6f6
Degranon <boba00099@gmail.com>
2025-07-13 23:17:52 +0200

    The edited messages are now correctly indicated as such

--- interface/scripted/starcustomchat/base/chat_class.lua
+++ interface/scripted/starcustomchat/base/chat_class.lua
@@ -604,7 +604,7 @@ function StarCustomChat:processQueue()
     
     -- If the message should contain an avatar and name:
     local prevDrawnMessage = self.messages[self.drawnMessageIndexes[i - 1]]
-    message.avatar = i == 1 or (message.connection ~= prevDrawnMessage.connection or message.mode ~= prevDrawnMessage.mode or message.nickname ~= prevDrawnMessage.nickname or message.portrait ~= prevDrawnMessage.portrait)
+    message.avatar = i == 1 or (message.connection ~= prevDrawnMessage.connection or message.mode ~= prevDrawnMessage.mode or message.nickname ~= prevDrawnMessage.nickname or message.portrait ~= prevDrawnMessage.portrait or message.edited)
 
 
     local text = self.chatMode == "modern" and message.text 
